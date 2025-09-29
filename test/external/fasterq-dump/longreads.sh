4bcf8a70b9be78c80c816a166ac9b2b5778a5c60
Andrew Klymenko <klymenka@ncbi.nlm.nih.gov>
2025-09-29 17:57:47 -0400

    VDB-6105: fixed to work on both Mac and Linux

--- test/external/fasterq-dump/longreads.sh
+++ test/external/fasterq-dump/longreads.sh
@@ -35,8 +35,7 @@ ACC="LONGREADS"
 
 OS=$(uname -s)
 #echo "$OS"
-if [ "$OS" == "Darwin" ]
-then
+if [ "$OS" == "Darwin" ] ; then
     MD5="/sbin/md5 -q"
 else
     MD5="md5sum"
@@ -79,10 +78,17 @@ ${MD5} "${ACC}_2.fastq" >> actual.txt
 # =============================================================================$
 # produce expected MD5-sums of the 2 FASTQ-files
 # =============================================================================$
+if [ "$OS" == "Darwin" ] ; then
+cat << EOF > expected.txt
+3f7af3bd91953afd553291708d5eb181
+6f42f3ec7449c54d810a4fbaa07aa19f
+EOF
+else
 cat << EOF > expected.txt
 24cc3655c79ef01c23d96cc5a595b138  LONGREADS_1.fastq
 d0b6299ab49f6dbc1ba42ec6056df70d  LONGREADS_2.fastq
 EOF
+fi
 
 # =============================================================================$
 # compare actual against expected
